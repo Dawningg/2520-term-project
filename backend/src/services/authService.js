d568a62ac35affe1100492fef247495d9087059b
Dawningg <michaelmehram@gmail.com>
2025-12-11 21:56:15 -0800

    uncommented const token and added more code

--- backend/src/services/authService.js
+++ backend/src/services/authService.js
@@ -9,6 +9,7 @@ export default {
     // TODO: get ahold of the db using readDb();
     const db = await readDb();
     // TODO: check if there is an existing user with the same username
+    const existingUser = db.users.find(user => user.username === username);
     // TODO: if there is, do the following:
     //       - construct a new Error("Username already taken");
     //       - set the statusCode of that error object to 400
@@ -43,13 +44,17 @@ export default {
     // TODO: get ahold of the db using readDb();
     const db = await readDb();
     // TODO: check the database for a user with a matching username and password
-    const user = db.users.find((u) => u.username === username && u.password === password);
+    const user = db.users.find(user => user.username === username && user.password === password);
     // TODO: if there is no user:
     //       - construct a new Error("Invalid username or password");
     //       - set the statusCode of that error object to 401
     //       - throw the err
+    if (!user) {const err = new Error("Invalid username or password")
+      err.statusCode = 401;
+      throw err
+    }
     // TODO: otherwise, create a login token. I'll help you out with this one:
-    // const token = jwt.sign({ userId: user.id, username: user.username }, JWT_SECRET, { expiresIn: "1h" })
+    const token = jwt.sign({ userId: user.id, username: user.username }, JWT_SECRET, { expiresIn: "1h" })
 
     // TODO:  return an object that contains 2 things:
     //  - token
